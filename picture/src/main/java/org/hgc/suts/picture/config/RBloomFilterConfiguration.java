a6c817581a155ad9efd2e76008a9c5ed8e3d8641
XieYiqiang <1103938364@qq.com>
2025-11-28 20:45:23 +0800

    feat: 为图片模块配置redis.

---
+++ picture/src/main/java/org/hgc/suts/picture/config/RBloomFilterConfiguration.java
@@ -0,0 +1,15 @@
+package org.hgc.suts.picture.config;
+
+import org.redisson.api.RBloomFilter;
+import org.redisson.api.RedissonClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * 布隆过滤器配置
+ */
+@Configuration
+public class RBloomFilterConfiguration {
+
+
+}
