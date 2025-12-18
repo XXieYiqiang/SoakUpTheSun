15f5e1abd66003d42c478b324c1059b9a3bd6d7d
XieYiqiang <1103938364@qq.com>
2025-12-18 19:40:55 +0800

    feat : 新增请求志愿者即时通讯联调.

--- shortlink/src/main/java/org/hgc/suts/shortlink/ShortlinkApplication.java
+++ shortlink/src/main/java/org/hgc/suts/shortlink/ShortlinkApplication.java
@@ -3,9 +3,11 @@ package org.hgc.suts.shortlink;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @MapperScan("org.hgc.suts.shortlink.dao.mapper")
+@EnableDiscoveryClient
 public class ShortlinkApplication {
 
     public static void main(String[] args) {
